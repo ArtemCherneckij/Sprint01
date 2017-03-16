d69589420372e21b7aa5e3e56447d102f09d82db
ArtemCherneckij <temich97@mail.ru>
2017-03-16 23:44:12 +0300

    instinctools_FourStep

--- TableUI/TableViewCell.m
+++ TableUI/TableViewCell.m
@@ -25,10 +25,10 @@
         // Initialization code
         self.name = [[UILabel alloc]init];
         self.name.textAlignment = UITextAlignmentLeft;
-        self.name.font = [UIFont systemFontOfSize:14];
+        self.name.font = [UIFont systemFontOfSize:20];
         self.subName = [[UILabel alloc]init];
         self.subName.textAlignment = UITextAlignmentLeft;
-        self.subName.font = [UIFont systemFontOfSize:8];
+        self.subName.font = [UIFont systemFontOfSize:10];
         self.image = [[UIImageView alloc]init];
         [self.contentView addSubview:self.name];
         [self.contentView addSubview:self.subName];
@@ -44,13 +44,13 @@
     CGFloat boundsX = contentRect.origin.x;
     CGRect frame;
     
-    frame= CGRectMake(boundsX+20 ,0, 43, 43);
+    frame= CGRectMake(boundsX+20 ,0, 40, 40);
     self.image.frame = frame;
     
     frame= CGRectMake(boundsX+70 ,5, 200, 25);
     self.name.frame = frame;
     
-    frame= CGRectMake(boundsX+70 ,30, 100, 15);
+    frame= CGRectMake(boundsX+70 ,25, 100, 15);
     self.subName.frame = frame;
 }
 
